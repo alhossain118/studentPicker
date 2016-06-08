b8b0d59dfa82b9cdb5dbfae527f6695dc9d17949
Alamin Hossain <alhossain118@gmail.com>
2016-06-08 13:21:18 -0700

    2nd shot

--- main.js
+++ main.js
@@ -5,41 +5,32 @@ var arr = [];
 function init(){
     document.getElementsByClassName("doStuff")[0].addEventListener('click', addTheStudent)
     document.getElementsByClassName("randomStudent")[0].addEventListener('click', randomName)
+  //  document.getElementsByClassName("setGroups")[0].addEventListener('click', groupNames)
 }
 function addTheStudent(){
   var name = document.getElementById("searchTxt").value;
+  console.log(name);
   var para = document.createElement('p');
-  name = name.split(" ");
+  name = name.split(",");
+  console.log("Line 14: " + name)
+  document.querySelector('.box').appendChild(para)
 
-  //console.log(name.split(' '));;
-  //for(var i = 0; i < name.length; i++){
   if(name.length > 1){
     for(var i = 0; i < name.length; i++){
       arr.push(name[i])
-      para.textContent += name[i] + ' ';
+      para.textContent +=  " " + name[i] + " "  ;
+    //  para.appendChild
       console.log("para", para);
-      // document.querySelector('.box').appendChild(para)
     }
   }
   else{
     arr.push(name)
     para.textContent = name;
-    // document.querySelector('.box').appendChild(para)
   }
-    //arr.push(name);
-document.querySelector('.box').appendChild(para)
 
-    //para.textContent = name[1];
-
-
-  //}
   console.log("arr", arr)
-  // para.textContent = name;
-  // document.querySelector('.box').appendChild(para)
-  // arr.push(name);
    document.getElementById("searchTxt").value = ""
 }
-
 function randomName (){
 var para = document.getElementsByClassName("randomStudentName")[0]
 var randomArray = arr[Math.floor(Math.random()*arr.length)];
@@ -47,3 +38,16 @@ var randomArray = arr[Math.floor(Math.random()*arr.length)];
 para.textContent = randomArray;
 console.log(randomArray)
 }
+
+// function groupNames (){
+//   var box = document.createElement('div');
+//   var group = document.getElementById("groupTxt").value;
+//   console.log(group)
+//   for(var i =0; i < arr; i++){
+//
+//     for(var j = 0; i < group / )
+//   }
+//   // if(arr.length  % 2 === 0){
+//   //   document.querySelector('.groups').appendChild(para)
+//   // }
+// }
